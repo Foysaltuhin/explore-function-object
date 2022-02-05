8669be3cfa21c8e6ac63d76f8b570a67fde24ea3
tuhin <tuhinfoysal1111@gmail.com>
2022-02-05 20:42:17 +0600

    commited

--- object-properties.js
+++ object-properties.js
@@ -4,5 +4,11 @@ var computer = {
     processor: 'intel i7'
 }
 // console.log(computer);
+// console.log(computer.processor);
+var computerPrice = computer.price;
 
-console.log(computer.processo);
+// set a object of value 
+var computerrrice = 22000;
+console.log(computer);
+// console.log(computerPrice)
+
---
+++ switch.js
@@ -0,0 +1,20 @@
+var color = 'yellow';
+
+if (color == 'blue') {
+    console.log('color is blue');
+}
+else if (color == 'red') {
+    console.log('color is red');
+}
+else if (color == 'green') {
+    console.log('color is green');
+}
+else if (color == 'whit') {
+    console.log('color is white');
+}
+else if (color == 'yellow') {
+    console.log('color is yellow');
+}
+else (
+    console.log('color is black')
+)
